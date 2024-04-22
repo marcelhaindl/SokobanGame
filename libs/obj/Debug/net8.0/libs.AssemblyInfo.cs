375af201c0d76d9ad2b3021176b041b434969d2d
LionRise <lenastadlinger@gmail.com>
2024-04-22 10:20:41 +0200

    test

--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1384355c0f8eab199c97f58d1c70ee469a5092e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a181d2159a8ceec8f7d98d5a20935bb131946eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
