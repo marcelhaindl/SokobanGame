a0107334cdb57fc0c2bfa855ced4a1975799c06d
cc221005 <cc221005@fhstp.ac.at>
2024-04-22 10:16:27 +0200

    Rewind and Levels merged

--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aafdb3e022b6703c6a113f2bb4acf19a3de3cb4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf750d3a72c66254c8c0c7edd953352cc9f79e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
